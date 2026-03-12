31e28f67080427eae118526a80f5c62a23d85405
s31696 <s31696@pjwstk.edu.pl>
2026-03-12 09:28:25 +0100

    loop upgrade

--- apbd-cw1-git-s31696/apbd-cw1-git-s31696/Program.cs
+++ apbd-cw1-git-s31696/apbd-cw1-git-s31696/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine(i);
+    Console.Write("i:" + i);
+    Console.Write(" i % 2? ");
+    Console.WriteLine(i % 2 == 0 ? "tak" : "nie");
+    
 }
