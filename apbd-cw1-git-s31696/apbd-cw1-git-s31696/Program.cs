d8ba321e51cabbd53aec73a7c4b31b18afab1044
s31696 <s31696@pjwstk.edu.pl>
2026-03-12 09:33:26 +0100

    new Array

--- apbd-cw1-git-s31696/apbd-cw1-git-s31696/Program.cs
+++ apbd-cw1-git-s31696/apbd-cw1-git-s31696/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int[] newtab = new int[10];
 for (int i = 0; i < 10; i++)
 {
     Console.Write("i:" + i);
     Console.Write(" i % 2? ");
     Console.WriteLine(i % 2 == 0 ? "tak" : "nie");
-    
+    newtab[i] = i;
 }
